925efb0aa7039b41128c7b7470adf4d1fc24569d
thopica <thomasmeister6@gmail.com>
2022-06-25 15:16:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+
+function generatePassword() {
+   let passwordOne = "";
+   let passwordTwo = "";
+   
+   for (let i = 0; i < 15; i++) {
+   passwordOne += characters[Math.floor(Math.random() * characters.length)]
+   passwordTwo += characters[Math.floor(Math.random() * characters.length)]
+    }
+    
+    document.getElementById("passwordone-el").textContent = passwordOne
+    document.getElementById("passwordtwo-el").textContent = passwordTwo
+
+}
+
+
+
+
+    
+    
+    
+
+    
+    
+
+
+
+
+
+
+
